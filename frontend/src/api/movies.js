b0a01a2fcadc13d2c339ea92df2c229e805821e7
keshav-hue <baplawatkeshav@gmail.com>
2026-06-22 15:19:42 +0530

    Configure API URL using Vite environment variables

--- frontend/src/api/movies.js
+++ frontend/src/api/movies.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5668";
+const API = import.meta.env.VITE_API_URL;
 
 export const getMovies = async (page = 1) => {
   const response = await axios.get(
